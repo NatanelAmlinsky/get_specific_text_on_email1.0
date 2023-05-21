8a39a62ae55624850c9b864094b555f3b6d0cd3a
NatanelAmlinsky <natanel.amlinsky@gmail.com>
2023-05-21 20:36:23 +0300

    basing all the info throu JSON file

--- book_orders.py
+++ book_orders.py
@@ -1,40 +1,43 @@
 import base_page
 from base_page import OutlookAccount
+import json
 
 
 class OrderBooksForm:
-
-    account_name = "netanel.amlinsky@jewsforjesus.org"
+    json_file = open('configuration.json', 'r', encoding='utf-8')  # open the JSON file
+    data = json.load(json_file)  # loading the JSON content to the variable "data"
+    account_name = data["account name"]
     outlook_account = OutlookAccount(account_name)
+
+
     if outlook_account.login():
-        # set up the sender name to filter by
-        sender_name = "Netanel Amlinsky"
-        sender_email = 'wordpress@yeshuanekuda.co.il'
+        # set up the sender name and email to filter by
+        senders_name = data["sender name"]
+        senders_email = data["senders email"]
         count = 0
-        forms_txt_subject = ["התקבלה בקשה להזמנת", "ביצע הזמנת ספרים", "התקבלה בקשה ליצירת קשר", "התקבלה בקשה לביקור"]
+        forms_txt_subject = data["forms txt subject"]
+
         # loop through the emails in the inbox folder
-        for message in outlook_account.inbox_folder.Items:  # Books orders form
-            # check if the message is from the specified sender name
+        for message in outlook_account.inbox_folder.Items:
             if message.Class == 43:
                 if message.SenderEmailType == "EX":
-                    if message.Sender.GetExchangeUser().PrimarySmtpAddress == sender_email:
-                        for text in forms_txt_subject:
-                            if text in message.Subject.lower():
-                                # get the email content
-                                parts = base_page.OutlookAccount.get_email_content(message)
-                                count += 1
-                                break
-
+                    for email in senders_email:
+                        if message.Sender.GetExchangeUser().PrimarySmtpAddress == email:
+                            for subject in forms_txt_subject:
+                                if subject in message.Subject.lower():
+                                    # get the email content
+                                    parts = base_page.OutlookAccount.get_email_content(message)
+                                    count += 1
+                                    break  # exit the inner loop
                 else:
-                    if message.SenderEmailAddress == sender_email:
-                        for text in forms_txt_subject:
-                            if text in message.Subject.lower():
-                                # get the email content
-                                parts = base_page.OutlookAccount.get_email_content(message)
-                                count += 1
-                                break
+                    for email in senders_email:
+                        if message.SenderEmailAddress == email:
+                            for subject in forms_txt_subject:
+                                if subject in message.Subject.lower():
+                                    # get the email content
+                                    parts = base_page.OutlookAccount.get_email_content(message)
+                                    count += 1
+                                    break  # exit the inner loop
 
-        print("Found {} emails from '{}'".format(count, sender_email))
-
-
+        print("Found {} emails from '{}'".format(count, senders_email))
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import book_orders
 
 print("hello")
 
-# book_orders.OrderBooksForm()  # New Testament books and other book orders form
+book_orders.OrderBooksForm()  # New Testament books and other book orders form
 
 
 
