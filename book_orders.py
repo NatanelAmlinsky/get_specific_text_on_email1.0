20caa6ce46613241fdaa0a9666dfc7023956047b
NatanelAmlinsky <natanel.amlinsky@gmail.com>
2023-06-29 17:39:22 +0300

    Extract method changed

--- book_orders.py
+++ book_orders.py
@@ -4,41 +4,47 @@ import json
 
 
 class OrderBooksForm:
-    json_file = open('configuration.json', 'r', encoding='utf-8')  # open the JSON file
-    data = json.load(json_file)  # loading the JSON content to the variable "data"
+    json_file = open('configuration.json', 'r', encoding='utf-8')
+    data = json.load(json_file)
     account_name = ""
     senders_email = ""
     count = 0
+
     for account_name_txt in data["account name"]:
         if account_name_txt in data["account name"]:
             account_name = account_name_txt
             outlook_account = OutlookAccount(account_name)
+
             if outlook_account.login():
-                # set up the sender name and email to filter by
                 senders_email = data["senders email"]
                 count = 0
                 forms_txt_subject = data["forms txt subject"]
 
-                # loop through the emails in the inbox folder
                 for message in outlook_account.inbox_folder.Items:
                     if message.Class == 43:
                         if message.SenderEmailType == "EX":
                             for email in senders_email:
-                                if message.Sender.GetExchangeUser().PrimarySmtpAddress == email:
+                                if email in message.SenderEmailAddress:
+                                    if email == "form-submission@squarespace.info":
+                                        parts = outlook_account.get_email_content(message)
+                                        count += 1
+                                        break
                                     for subject in forms_txt_subject:
                                         if subject in message.Subject.lower():
-                                            # get the email content
                                             parts = outlook_account.get_email_content(message)
                                             count += 1
-                                            break  # exit the inner loop
+                                            break
                         else:
                             for email in senders_email:
-                                if message.SenderEmailAddress == email:
+                                if email in message.SenderEmailAddress:
+                                    if email == "form-submission@squarespace.info":
+                                        parts = outlook_account.get_email_content(message)
+                                        count += 1
+                                        break
                                     for subject in forms_txt_subject:
                                         if subject in message.Subject.lower():
-                                            # get the email content
                                             parts = outlook_account.get_email_content(message)
                                             count += 1
-                                            break  # exit the inner loop
+                                            break
 
     print("Found {} emails from '{}'".format(count, senders_email), account_name)
